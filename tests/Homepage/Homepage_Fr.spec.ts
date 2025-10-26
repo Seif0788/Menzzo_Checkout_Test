27334e196b243471e5e0f84f68114851e07dc514
Seifeddine Tajouri <seifeddine.t@axelites.com>
2025-10-26 11:37:45 +0100

    Update Playwright workflow, add cleanup workflow, and Homepage tests

---
+++ tests/Homepage/Homepage_Fr.spec.ts
@@ -0,0 +1,13 @@
+import {test, expect} from '@playwright/test';
+
+test('Menzzo.fr homepage should load correctly', async ({page})=>{
+
+    //Check that the homepage loads correctly
+    await page.goto('https://www.menzzo.fr/');
+
+    //Check that the title contains "Menzzo"
+    await expect(page).toHaveTitle(/Menzzo/);
+
+    //Check the main logo
+    await expect(page.locator('img[alt="Menzzo"]')).toBeVisible();
+})
